bc462586ceee3fb183f97a90bf5881ad800f9dbd
Joel Tanner Jones <jonesjoe@ecu.edu>
2018-04-30 10:44:39 -0400

    Restrict InstitutionalUnit resource routes
    Only create routes for index, show, new, create

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'institutional_units#index'
-  resources :institutional_units, param: :slug, path: 'institutional-units'
+  resources :institutional_units, only: [:index, :show, :new, :create], param: :slug, path: 'institutional-units'
 end
